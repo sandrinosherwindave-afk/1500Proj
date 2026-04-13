312801c4e4b17794c4781a07ef5c65a2ebc95e25
shee._.win <sandrino.sherwindave@gmail.com>
2026-04-13 13:13:21 +0800

    Add gameplay frame navigation and story progression; implement frame switching and button for next frame

--- gameplay.py
+++ gameplay.py
@@ -30,6 +30,7 @@ class FirstYear:
         bg = "black"
         )
         
+
         def story1_animation(index, Year1story_1):
             fullintro_story1 = "Day 1\n You start your day when you encountered a professor"
             
@@ -47,6 +48,45 @@ class FirstYear:
         
         Year1story_1.pack(pady = 10)            
         story1_animation(index, Year1story_1)
+        self.start_gameplay()
+        
+    def start_gameplay(self):
+
+        def frame():
+            def show_frame(frame):
+                """Raises the specified frame to the front."""
+                frame.tkraise()
+                        
+            frame = tk.Frame(self.master)
+            frame.pack(fill = "both", expand = True)
+            
+            battle1 = tk.Frame(frame, bg = "lightblue")
+            tk.Label(battle1, text="This is Page 1").pack()
+            tk.Button(battle1, text="Next", command=lambda: show_frame(battle2)).pack()
+            
+            battle2 = tk.Frame(frame, bg = "lightgreen")
+            tk.Label(battle2, text="This is Page 2").pack()
+            tk.Button(battle2, text="Next", command=lambda: show_frame(battle1)).pack()
+            
+            for F in (battle1, battle2):
+                F.grid(row=0, column=0, sticky="nsew")
+            
+                
+            show_frame(battle1)
+                
+        game_start = tk.Button(
+        self.master,
+        font = ("Courier", 10),
+        fg = "white",
+        bg = "black",
+        relief = "groove",
+        command = frame,
+        text = "Show next frame"
+        )
+        
+        game_start.pack(pady = 10)
+        
+           
 
 
         
